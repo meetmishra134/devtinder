be025981aac2430a18874e7536e9cde11474b771
Meet Mishra <mishrameet143@gmail.com>
2025-10-13 12:12:54 +0530

    Rest api completed

--- src/app.js
+++ src/app.js
@@ -57,17 +57,63 @@ const app = express();
 // });
 const { connectDb } = require("./config/database");
 const { User } = require("./models/user");
+
+app.use(express.json());
+
+//Add a user to the database
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Amit",
-    lastName: "sharma",
-    email: "amitsharma@example.com",
-    password: "secure143",
-    age: 25,
-    gender: "Male",
-  });
-  await user.save();
-  res.send("User added successfully");
+  // console.log(req.body);
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send("User added successfully");
+  } catch (error) {
+    res.status(500).send("Error adding user");
+  }
+});
+//Get the user from the database by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.find({ email: userEmail });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(201).send(user);
+    }
+  } catch (error) {
+    res.status(500).send("Error fetching user");
+  }
+});
+
+//Get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(401).send("Error fetching data");
+  }
+});
+//Delete the user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.id;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.status(201).send("User Deleted Successfully");
+  } catch (error) {
+    res.status(500).send("Error deleting user");
+  }
+});
+//Update the user in the database
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: data._id }, data);
+    res.status(201).send("User Updated Successfully");
+  } catch (error) {
+    res.status(500).send("Error updating user");
+  }
 });
 
 connectDb()
